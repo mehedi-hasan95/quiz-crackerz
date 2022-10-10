8f5cd7e747950aab31640e18ddbc0da50aabc50f
mehedititu <mh.me.mehedi@gmail.com>
2022-10-10 21:22:39 +0600

    Error page done

--- src/layouts/router.jsx
+++ src/layouts/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../components/Blog/Blog";
+import Error from "../components/Error/Error";
 import Home from "../components/Home/Home";
 import Topics from "../components/Topics/Topics";
 import Main from "./Main";
@@ -11,7 +12,8 @@ const router = createBrowserRouter([
         {path: '/home', element: <Home></Home>},
         {path: '/topics', element: <Topics></Topics>},
         {path: '/blog', element: <Blog></Blog>}
-    ]
+    ],
+    errorElement: <Error></Error>
 }
 ]);
 
